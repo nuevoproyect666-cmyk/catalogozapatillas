01860d6b30eeb7fd4dee7d803570fb15848d1c7c
nuevoproyect666-cmyk <nuevoproyect666@gmail.com>
2026-01-28 19:26:41 -0300

    Create catalogo.js

---
+++ js/catalogo.js
@@ -0,0 +1,27 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const contenedor = document.getElementById("productos");
+
+async function cargarProductos() {
+  const querySnapshot = await getDocs(collection(db, "productos"));
+
+  querySnapshot.forEach((doc) => {
+    const p = doc.data();
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <h3>${p.nombre}</h3>
+      <p>Marca: ${p.marca}</p>
+      <p>Precio: $${p.precio}</p>
+      <img src="${p.imagen}" width="200">
+      <hr>
+    `;
+    contenedor.appendChild(div);
+  });
+}
+
+cargarProductos();
