f5939c79f3356a49a1dbf03f67b9409f60805636
nuevoproyect666-cmyk <nuevoproyect666@gmail.com>
2026-01-28 19:13:14 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,21 @@
+// Importar Firebase desde CDN (modo módulo)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBSHZzduDrWT-CFmoYOit6euHhpTIeuWwQ",
+  authDomain: "catalogo-zapatillas-88d20.firebaseapp.com",
+  projectId: "catalogo-zapatillas-88d20",
+  storageBucket: "catalogo-zapatillas-88d20.firebasestorage.app",
+  messagingSenderId: "362371368674",
+  appId: "1:362371368674:web:c3810ede87f8cdb28c40bb"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar servicios para usarlos en otros archivos
+export const db = getFirestore(app);
+export const auth = getAuth(app);
